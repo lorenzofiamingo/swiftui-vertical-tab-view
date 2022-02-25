ed4a9d7c2263af5d7229f6fedc6bfa331f9aedda
Lorenzo Fiamingo <l99fiamingo@gmail.com>
2022-02-25 21:12:36 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,15 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftUI-VerticalTabView",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
         .library(
             name: "VTabView",
